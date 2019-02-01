5b297873932c8b96d0b061bf65f55ce615eeed75
ma zhenyuan <47215432+pykalabo@users.noreply.github.com>
2019-02-01 16:33:02 +0800

    Create Demo1.py
    Tensorflow env test

---
+++ TensorFlow/Demo1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+
+print(tf.__version__) 
